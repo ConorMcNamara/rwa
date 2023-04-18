aa5a742679cc8e13e77375ad80efd5df258a2960
Conor McNamara <conor.s.mcnamara@gmail.com>
2023-04-17 18:29:15 -0700

    Update test_rwa.py

--- test/test_rwa.py
+++ test/test_rwa.py
@@ -14,8 +14,8 @@ class TestRWA:
         df = sm.datasets.get_rdataset("mtcars", "datasets", cache=True).data
         mtcars = pd.DataFrame(df)
         weights = johnson_relative_weights(mtcars, ["cyl", "disp", "hp", "gear"], "mpg")
-        actual_weight_array = weights.loc["relative weights", :].to_numpy()
-        actual_rescaled_array = weights.loc["rescaled relative weights", :].to_numpy()
+        actual_weight_array = weights.loc[:, "relative weights"].to_numpy()
+        actual_rescaled_array = weights.loc[:, "rescaled relative weights"].to_numpy()
         # https://martinctc.github.io/rwa/ for comparison
         expected_weight_array = np.array([0.2284797, 0.2221469, 0.2321744, 0.0963886])
         expected_rescaled_array = np.array([29.32274, 28.50999, 29.79691, 12.37037])
